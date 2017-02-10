144e21ad46f3aac71f0a30821d3cdc52e80dbb0c
William B <blanchettwa@g.cofc.edu>
2017-02-10 07:03:32 -0500

    Began Work on Intents and Data Persistance

--- app/src/main/java/tk/wablanchett/mortgage_calculator_blanchett/DataEntry.java
+++ app/src/main/java/tk/wablanchett/mortgage_calculator_blanchett/DataEntry.java
@@ -1,14 +1,18 @@
 package tk.wablanchett.mortgage_calculator_blanchett;
 
+import android.content.Intent;
 import android.icu.util.Calendar;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -47,6 +51,7 @@ public class DataEntry extends AppCompatActivity {
     boolean iPYassign = false;
 
 
+
     DatePicker pickDate;
     Calendar calendar;
     TextView startDate;
@@ -67,6 +72,20 @@ public void upDateLabel(){
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_entry);
+/**
+ Intent transvalue = getIntent();
+ transvalue.getDoubleExtra("HV",homeValue);
+ transvalue.getDoubleExtra("LA",loanAmount);
+ transvalue.getDoubleExtra("IR",interestRate);
+ transvalue.getIntExtra("LT",loanTerm);
+ transvalue.getIntExtra("SM",startMonth);
+ transvalue.getIntExtra("SY",startYear);
+ transvalue.getDoubleExtra("PT",propertyTax);
+ transvalue.getDoubleExtra("IPY",insurancePerYear);
+ transvalue.getDoubleExtra("MHOA",monthlyHOA);
+
+ */
+        Intent restore = getIntent();
 
         startDate = (TextView) findViewById(R.id.startDateLabel);
         calendar = Calendar.getInstance();
@@ -80,6 +99,12 @@ public void upDateLabel(){
             }
         });
 
+        System.out.println(pickDate.getMonth());
+        System.out.println(pickDate.getYear());
+        System.out.println("THIS WAS A TEST ______________________________");
+
+        gotoMortgageSummary = (Button) findViewById(R.id.mortgageSummaryButton);
+        gotoPaymentSummary = (Button) findViewById(R.id.paymentSummaryButton);
 
         hVEdit = (EditText) findViewById(R.id.homeValue);
         hVEdit.addTextChangedListener(new TextWatcher() {
@@ -97,7 +122,7 @@ public void upDateLabel(){
             public void afterTextChanged(Editable s) {
 
                 if (s.length() > 0)
-                {homeValue = Double.valueOf(s.toString()); hVassigned = true;}
+                {homeValue = Double.valueOf(("0" + s.toString())); hVassigned = true;}
                 else{hVassigned = false; homeValue = 0;}
 
                 System.out.print("Set the value to ");
@@ -119,7 +144,7 @@ public void upDateLabel(){
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (s.length() > 0) {loanAmount = Double.valueOf(s.toString()); lAassigned = true;}
+                if (s.length() > 0) {loanAmount = Double.valueOf(("0" + s.toString())); lAassigned = true;}
                 else{lAassigned = false;loanAmount = 0;}
 
                 System.out.print("Set the value to ");
@@ -176,7 +201,7 @@ public void upDateLabel(){
 
             @Override
             public void afterTextChanged(Editable s) { if (s.length() > 0){
-                interestRate = Double.valueOf(s.toString());
+                interestRate = Double.valueOf(("0" + s.toString()));
                 iRassign = true;
             }
                 else{iRassign = false; interestRate = 0;}
@@ -202,7 +227,7 @@ public void upDateLabel(){
             public void afterTextChanged(Editable s) {
 
                 if (s.length() > 0){
-                    propertyTax = Double.valueOf(s.toString());
+                    propertyTax = Double.valueOf(("0" + s.toString()));
                     pTassign = true;
                 }
                 else{pTassign = false; propertyTax = 0;}
@@ -226,7 +251,7 @@ public void upDateLabel(){
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (s.length() > 0) {monthlyHOA = Double.valueOf(s.toString()); mHOAassign = true;}
+                if (s.length() > 0) {monthlyHOA = Double.valueOf(("0" + s.toString())); mHOAassign = true;}
                 else{mHOAassign = false; monthlyHOA = 0;}
 
                 System.out.print("Set the value to ");
@@ -249,7 +274,7 @@ public void upDateLabel(){
 
             @Override
             public void afterTextChanged(Editable s) {
-            if (s.length() > 0){ insurancePerYear = Double.valueOf(s.toString()); iPYassign = true;}
+            if (s.length() > 0){ insurancePerYear = Double.valueOf(("0" + s.toString())); iPYassign = true;}
                 else{insurancePerYear = 0; iPYassign = false;}
 
                 System.out.print("Set the value to ");
@@ -257,8 +282,66 @@ public void upDateLabel(){
             }
         });
 
+        gotoMortgageSummary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((hVassigned && lAassigned && lTassigned && iRassign && pTassign && mHOAassign && iPYassign) == true)
+                {
+                    Intent mortgageSummary = new Intent(DataEntry.this, MortgageSummary.class);
+                    mortgageSummary.putExtra("HV",homeValue);
+                    mortgageSummary.putExtra("LA", loanAmount);
+                    mortgageSummary.putExtra("IR",interestRate);
+                    mortgageSummary.putExtra("LT",loanTerm);
+                    mortgageSummary.putExtra("SM",(pickDate.getMonth() + 1));
+                    mortgageSummary.putExtra("SY", (pickDate.getYear()));
+                    mortgageSummary.putExtra("PT", propertyTax);
+                    mortgageSummary.putExtra("IPY", insurancePerYear);
+                    mortgageSummary.putExtra("MHOA", monthlyHOA);
+                    DataEntry.this.startActivity(mortgageSummary);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Please provide all values",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+
+        gotoPaymentSummary.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                if ((hVassigned && lAassigned && lTassigned && iRassign && pTassign && mHOAassign && iPYassign) == true)
+                {
+                    Intent paymentSummary = new Intent(DataEntry.this, PaymentSummary.class);
+                    paymentSummary.putExtra("HV",homeValue);
+                    paymentSummary.putExtra("LA", loanAmount);
+                    paymentSummary.putExtra("IR",interestRate);
+                    paymentSummary.putExtra("LT",loanTerm);
+                    paymentSummary.putExtra("SM",(pickDate.getMonth() + 1));
+                    paymentSummary.putExtra("SY", (pickDate.getYear()));
+                    paymentSummary.putExtra("PT", propertyTax);
+                    paymentSummary.putExtra("IPY", insurancePerYear);
+                    paymentSummary.putExtra("MHOA", monthlyHOA);
+                    DataEntry.this.startActivity(paymentSummary);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Please provide all values",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        );
 
 
 
-    }
-}
+    } //end of onCreate
+
+
+
+} //end of class
--- app/src/main/java/tk/wablanchett/mortgage_calculator_blanchett/PaymentSummary.java
+++ app/src/main/java/tk/wablanchett/mortgage_calculator_blanchett/PaymentSummary.java
@@ -1,13 +1,36 @@
 package tk.wablanchett.mortgage_calculator_blanchett;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PaymentSummary extends AppCompatActivity {
 
+    double homeValue;
+    double loanAmount;
+    int loanTerm;
+    double interestRate;
+    double propertyTax;
+    double monthlyHOA;
+    double insurancePerYear;
+    int startMonth;
+    int startYear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_summary);
+
+        Intent transvalue = getIntent();
+        transvalue.getDoubleExtra("HV",homeValue);
+        transvalue.getDoubleExtra("LA",loanAmount);
+        transvalue.getDoubleExtra("IR",interestRate);
+        transvalue.getIntExtra("LT",loanTerm);
+        transvalue.getIntExtra("SM",startMonth);
+        transvalue.getIntExtra("SY",startYear);
+        transvalue.getDoubleExtra("PT",propertyTax);
+        transvalue.getDoubleExtra("IPY",insurancePerYear);
+        transvalue.getDoubleExtra("MHOA",monthlyHOA);
+
     }
 }
